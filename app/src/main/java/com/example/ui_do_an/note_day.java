9102d85bcdb5ec2b87b0f3d4d799102ea91f9056
bananaa03 <21520643@gm.uit.edu.vn>
2023-11-21 22:29:25 +0700

    edit UI note_day

--- app/src/main/java/com/example/ui_do_an/note_day.java
+++ app/src/main/java/com/example/ui_do_an/note_day.java
@@ -15,7 +15,7 @@ public class note_day extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.note_day);
 
         buttonSetting = (ImageButton) findViewById(R.id.ImageButtonSetting);
         buttonSetting.setOnClickListener(new View.OnClickListener() {
