49784bac9ca8220a4481c7e1b6cdcdabbdcc2d1d
CaoKimCam <21520641@gm.uit.edu.vn>
2023-12-08 13:28:27 +0700

    Run app
    Tạo intent imageview.click qua day_night

--- app/src/main/java/com/example/ui_do_an/MainActivity.java
+++ app/src/main/java/com/example/ui_do_an/MainActivity.java
@@ -5,14 +5,23 @@ import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.myImageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onImageView(v);
+            }
+        });
     }
     public void onImageView(View view){
 
