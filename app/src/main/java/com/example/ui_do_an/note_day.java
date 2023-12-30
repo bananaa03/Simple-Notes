91dfb2bce178c4984ce77742fa652fcd629fb8dd
CaoKimCam <21520641@gm.uit.edu.vn>
2023-12-30 21:49:44 +0700

    update note_day.xml note_day.java
    btn_share trên gmail, tin nhắn

--- app/src/main/java/com/example/ui_do_an/note_day.java
+++ app/src/main/java/com/example/ui_do_an/note_day.java
@@ -4,22 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class note_day extends AppCompatActivity {
 
-    private ImageButton buttonSetting;
+    private ImageButton buttonSetting, btnShare;
+    EditText edtnotetitle, edtnotecontent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_day);
+        edtnotetitle = (EditText) findViewById(R.id.edt_note_title);
+        edtnotecontent= (EditText) findViewById(R.id.edt_note_content);
 
         buttonSetting = (ImageButton) findViewById(R.id.ImageButtonSetting);
+        btnShare = (ImageButton) findViewById(R.id.imgbtn_share);
         buttonSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openSetting();
+            }
+        });
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String noteTitle = edtnotetitle.getText().toString();
+                String noteContent = edtnotecontent.getText().toString();
+                String fullNote = noteTitle+"Chủ nhật"+"\n\n"+ noteContent+"Hi";
+
+                //Tạo intent chia sẻ nội dung
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, fullNote);
+
+                startActivity(Intent.createChooser(shareIntent,"Chia sẻ nội dung ghi chú"));
             }
         });
     }
--- app/src/main/java/com/example/ui_do_an/setting_day.java
+++ app/src/main/java/com/example/ui_do_an/setting_day.java
@@ -3,15 +3,19 @@ package com.example.ui_do_an;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class setting_day extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_day);
+
+
     }
     public void btnBacktoMain_day(View view){
         Intent intent = new Intent(this, day_main.class);
